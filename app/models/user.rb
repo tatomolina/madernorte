9804be9a29f38073181bf451e5481e9e31fc7b3a
Tato <gastomolina91@gmail.com>
2018-03-25 11:29:13 -0300

    Removed sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :orders       
+  has_many :orders
 end
