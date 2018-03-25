d300e03e754dfe7b3df93c9b0c3713a602630f8f
Tato <gastomolina91@gmail.com>
2018-03-25 11:44:52 -0300

    Removed registrable and recoverable from devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
   has_many :orders
 end
