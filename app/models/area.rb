34d3b49385794e270886722bbc0d577869eeb5e3
Tato <gastomolina91@gmail.com>
2018-03-26 19:50:06 -0300

    fixed areas bug

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,6 +1,6 @@
 class Area < ApplicationRecord
   validates :name, :presence => true
-  has_many :orders, inverse_of: :Area
+  has_many :orders
 
   def not_delivered
     self.orders.select{|x| x.delivered? == false}
