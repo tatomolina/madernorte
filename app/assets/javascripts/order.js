4c962c431efbbb4b928e338e9dcbb1e3729555f5
Tato <gastomolina91@gmail.com>
2018-04-08 01:35:32 -0300

    Fixed some bugs

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -2,7 +2,7 @@ $(document).on('turbolinks:load', function() {
 
   $('#order_client_id').on('change', function() {
     var clientName = $('#order_client_id').find(":selected").text();
-    if (clientName == "Consumidor final") {
+    if (clientName == "CONSUMIDOR FINAL") {
       $('#common-client').slideDown(200);
     } else {
       $('#common-client').slideUp(200);
