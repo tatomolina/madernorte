902d34d5cdf3e669543e8585d37c1b03b85549ac
Tato <gastomolina91@gmail.com>
2018-04-09 23:59:40 -0300

    Added check all

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -8,4 +8,24 @@ $(document).on('turbolinks:load', function() {
       $('#common-client').slideUp(200);
     };
   });
+
+  $("#checkAllDone").on('change', function() {
+    $(".order_articles_done").children().children().children().prop("checked", $(this).prop("checked"))
+  });
+
+  $("#checkAllDelivered").on('change', function() {
+    $(".order_articles_delivered").children().children().children().prop("checked", $(this).prop("checked"))
+  });
+
+  $("#checkAllInvoiced").on('change', function() {
+    $(".order_articles_invoiced").children().children().children().prop("checked", $(this).prop("checked"))
+  });
+
+  $("#checkAllPayed").on('change', function() {
+    $(".order_articles_payed").children().children().children().prop("checked", $(this).prop("checked"))
+  });
+
+  $("#checkAllProcess").on('change', function() {
+    $(".order_articles_process").children().children().children().prop("checked", $(this).prop("checked"))
+  });
 });
